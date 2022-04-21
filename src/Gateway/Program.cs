612d000538e4110b48e2c4be22e839ae1d95e21a
Thorsten Hans <thorsten.hans@gmail.com>
2022-04-21 15:46:59 +0200

    chore: add OpenTelemetry integration with Gateway
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -1,15 +1,26 @@
+using Gateway;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddTransforms<DaprRequestTransformProvider>();
+builder.Services.AddHealthChecks();
 builder.Services.AddOpenTelemetryTracing(b =>
-{
+{ 
     b.AddAspNetCoreInstrumentation()
-        .AddJaegerExporter();
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(typeof(Program).Assembly.GetName().Name))
+        .AddHttpClientInstrumentation()
+        .AddConsoleExporter()
+        .AddZipkinExporter(cfg =>{
+            // todo: extract uri to configuration
+            cfg.Endpoint = new Uri("http://localhost:9412/api/v2/spans");
+        });
 });
 var app = builder.Build();
 app.MapReverseProxy();
-
+app.MapHealthChecks("/healthz");
 app.Run();
