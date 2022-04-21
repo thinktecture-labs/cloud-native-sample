b54d763185b553b12adb1eba4348e36cad66c764
Thorsten Hans <thorsten.hans@gmail.com>
2022-04-21 11:53:51 +0200

    chore: add OpenTelemetry with Jaeger exporter
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -1,6 +1,14 @@
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddOpenTelemetryTracing(b =>
+{
+    b.AddAspNetCoreInstrumentation()
+        .AddJaegerExporter();
+});
 var app = builder.Build();
 app.MapReverseProxy();
 
