c0f39db1210924539e6f02ade77664fc12b97553
Christian Weyer <christian.weyer@thinktecture.com>
2022-04-26 14:26:08 +0200

    First version of the Blazor WASM client

---
+++ src/OrderMonitorClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using OrderMonitorClient;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress =
+    new Uri("http://localhost:5001/") });
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
