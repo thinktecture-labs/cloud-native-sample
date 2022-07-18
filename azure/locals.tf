b4a713fa802052826c3ffa646170f04ccb349365
Thorsten Hans <thorsten.hans@gmail.com>
2022-07-18 16:57:17 +0200

    chore: respect Azure Policies 😇
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- azure/locals.tf
+++ azure/locals.tf
@@ -1,7 +1,7 @@
 locals {
   default_tags = {
     "com.thinktecture.project" = "Cloud-Native Sample"
-    "com.thinktecture.author"  = "Thorsten Hans"
+    "com.thinktecture.owner"  = "Thorsten Hans"
   }
   tags = merge(local.default_tags, var.custom_tags)
 }
