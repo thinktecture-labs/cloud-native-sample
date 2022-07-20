e3dfba940f182ca80f7070f81a52826024f4ae9b
Thorsten Hans <thorsten.hans@gmail.com>
2022-07-20 10:51:32 +0200

    chore: Add cert-manager to k8s
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- iac/aks/modules/k8s/ingress.tf
+++ iac/aks/modules/k8s/ingress.tf
@@ -5,3 +5,12 @@ resource "helm_release" "ingress" {
   namespace        = "ingress"
   create_namespace = true
 }
+
+
+resource "helm_release" "certmanager" {
+  name             = "cn-cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+}
