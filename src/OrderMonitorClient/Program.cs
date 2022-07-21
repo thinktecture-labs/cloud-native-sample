867c3b19a3ed207cf384e2e54f7760f73671dc36
Christian Weyer <christian.weyer@thinktecture.com>
2022-07-21 16:46:24 +0200

    Adding Blazor authN (WIP)

--- src/OrderMonitorClient/Program.cs
+++ src/OrderMonitorClient/Program.cs
@@ -3,11 +3,24 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OrderMonitorClient;
 using MudBlazor.Services;
 using OrderMonitorClient.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("OrderMonitor", c =>
+    c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(services => services.GetRequiredService<IHttpClientFactory>()
+    .CreateClient("OrderMonitor"));
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Oidc", options.ProviderOptions);
+});
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress =
     new Uri(builder.HostEnvironment.BaseAddress) });
