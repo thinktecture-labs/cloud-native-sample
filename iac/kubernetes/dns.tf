1fa44dbbc5d69608cf043aa15efa10992ea65d37
Thorsten Hans <thorsten.hans@gmail.com>
2022-09-12 17:37:20 +0200

    chore: add dns record for cn-zipkin
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- iac/kubernetes/dns.tf
+++ iac/kubernetes/dns.tf
@@ -15,3 +15,12 @@ resource "azurerm_dns_a_record" "grafana" {
   ttl                 = 300
   records             = [data.kubernetes_service.ing.status.0.load_balancer.0.ingress.0.ip]
 }
+
+resource "azurerm_dns_a_record" "zipkin" {
+  count               = var.dns_zone.deploy_dns ? 1 : 0
+  name                = "cn-zipkin"
+  zone_name           = var.dns_zone.name
+  resource_group_name = var.dns_zone.resource_group_name
+  ttl                 = 300
+  records             = [data.kubernetes_service.ing.status.0.load_balancer.0.ingress.0.ip]
+}
