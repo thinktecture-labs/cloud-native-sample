45224fb1868add59cd6804f4ef42d58072d5e31c
Thorsten Hans <thorsten.hans@gmail.com>
2022-10-04 19:02:29 +0200

    chore: update HttpClient bootstraping
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/OrderMonitorClient/Program.cs
+++ src/OrderMonitorClient/Program.cs
@@ -31,25 +31,25 @@ builder.Logging.AddConfiguration(
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("OrderMonitor")
-    .AddHttpMessageHandler(sp =>
-    {
-        var handler = sp.GetService<AuthorizationMessageHandler>()
-            .ConfigureHandler(
-                authorizedUrls: new[] { builder.Configuration.GetValue<string>("ApiRoot") });
-        return handler;
-    });
+// builder.Services.AddHttpClient("OrderMonitor")
+//     .AddHttpMessageHandler(sp =>
+//     {
+//         var handler = sp.GetService<AuthorizationMessageHandler>()
+//             .ConfigureHandler(
+//                 authorizedUrls: new[] { builder.Configuration.GetValue<string>("ApiRoot") });
+//         return handler;
+//     });
 
-builder.Services.AddScoped(services => services.GetRequiredService<IHttpClientFactory>()
-    .CreateClient("OrderMonitor"));
+// builder.Services.AddScoped(services => services.GetRequiredService<IHttpClientFactory>()
+//     .CreateClient("OrderMonitor"));
 
 builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Oidc", options.ProviderOptions);
 });
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress =
-//    new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress =
+    new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<OrderMonitorService>();
 
