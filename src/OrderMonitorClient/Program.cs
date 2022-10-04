1067c517cb3b5e0164108a44cb07f8e507ef710e
Thorsten Hans <thorsten.hans@gmail.com>
2022-10-04 19:10:29 +0200

    chore: update Blazor HttpClient config to come with base address and authn middleware
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/OrderMonitorClient/Program.cs
+++ src/OrderMonitorClient/Program.cs
@@ -31,28 +31,20 @@ builder.Logging.AddConfiguration(
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddHttpClient("OrderMonitor")
-//     .AddHttpMessageHandler(sp =>
-//     {
-//         var handler = sp.GetService<AuthorizationMessageHandler>()
-//             .ConfigureHandler(
-//                 authorizedUrls: new[] { builder.Configuration.GetValue<string>("ApiRoot") });
-//         return handler;
-//     });
+builder.Services
+    .AddHttpClient("OrderMonitor",
+        client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddScoped(services => services.GetRequiredService<IHttpClientFactory>()
+    .CreateClient("OrderMonitor"));
 
-// builder.Services.AddScoped(services => services.GetRequiredService<IHttpClientFactory>()
-//     .CreateClient("OrderMonitor"));
 
 builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Oidc", options.ProviderOptions);
 });
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress =
-    new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddScoped<OrderMonitorService>();
-
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
