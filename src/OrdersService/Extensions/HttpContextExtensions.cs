498b2c69adb37b55009d3782814a2552f9ed6804
Thorsten Hans <thorsten.hans@gmail.com>
2022-10-05 12:00:24 +0200

    chore: Implement GetUserName and GetUserId
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/OrdersService/Extensions/HttpContextExtensions.cs
+++ src/OrdersService/Extensions/HttpContextExtensions.cs
@@ -1,16 +1,19 @@
-﻿namespace OrdersService.Extensions;
+﻿using System.Security.Claims;
+using IdentityModel;
+
+namespace OrdersService.Extensions;
 
 public static class HttpContextExtensions
 {
     public static string GetUserName(this HttpContext c)
     {
-        //TODO: extract from (claim) token 
-        return "John Doe";
+        var nameClaim = c.User.FindFirst(JwtClaimTypes.Name);
+        return nameClaim != null ? nameClaim.Value : "N/A";
     }
 
     public static string GetUserId(this HttpContext c)
     {
-        //TODO: extract from (claim) token
-        return Guid.Empty.ToString();
+        var subClaim = c.User.FindFirst(JwtClaimTypes.Subject);
+        return subClaim != null ? subClaim.Value : "N/A";
     }
 }
