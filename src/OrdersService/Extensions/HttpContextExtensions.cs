a569a38b801d8ea3a2cc404cce6c3c602e0ee950
Thorsten Hans <thorsten.hans@gmail.com>
2022-10-05 13:04:30 +0200

    chore: Use ClaimTypes instead of JwtClaimTypes
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/OrdersService/Extensions/HttpContextExtensions.cs
+++ src/OrdersService/Extensions/HttpContextExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Security.Claims;
-using IdentityModel;
 
 namespace OrdersService.Extensions;
 
@@ -7,13 +6,13 @@ public static class HttpContextExtensions
 {
     public static string GetUserName(this HttpContext c)
     {
-        var nameClaim = c.User.FindFirst(JwtClaimTypes.Name);
+        var nameClaim = c.User.FindFirst(ClaimTypes.Name);
         return nameClaim != null ? nameClaim.Value : "N/A";
     }
 
     public static string GetUserId(this HttpContext c)
     {
-        var subClaim = c.User.FindFirst(JwtClaimTypes.Subject);
+        var subClaim = c.User.FindFirst(ClaimTypes.NameIdentifier);
         return subClaim != null ? subClaim.Value : "N/A";
     }
 }
