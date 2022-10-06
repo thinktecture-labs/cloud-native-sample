4d61a379ad2abe939fedbd0c23f200ec5539b369
Thorsten Hans <thorsten.hans@gmail.com>
2022-10-06 11:46:21 +0200

    chore: demo dry
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/PriceWatcherService/Program.cs
+++ src/PriceWatcherService/Program.cs
@@ -1,10 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
 using PriceWatcher.Repositories;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Logging
+builder.Logging.ClearProviders();
+// always log to STDOUT 
+builder.Logging.AddConsole(options =>
+{
+    options.FormatterName = ConsoleFormatterNames.Json;
+});
+
+// instrumentation
+
+//traces
+builder.Services.AddOpenTelemetryTracing(options =>
+{
+    options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("PriceWatcher"))
+        .AddAspNetCoreInstrumentation()
+        .AddZipkinExporter()
+        .AddConsoleExporter();
+});
+
+// metrics
+builder.Services.AddOpenTelemetryMetrics(options =>
+{
+    options.ConfigureResource(rb =>
+        {
+            rb.AddService("PriceWatcher");
+        })
+        .AddRuntimeInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddPrometheusExporter();
+});
 
 // Add services to the container.
 builder.Services.AddSingleton<IPriceWatcherRepository, PriceWatcherRepository>();
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,10 +58,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+// decide if you want to check just this component or all dependencies too!?
+// both are valid...
+app.MapHealthChecks("/healthz/readiness");
+app.MapHealthChecks("/healthz/liveness");
+
+// expose metrics for prometheus scraping
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.Run();
