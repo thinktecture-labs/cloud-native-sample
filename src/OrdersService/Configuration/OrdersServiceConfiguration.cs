b4e2fdfc235af842065ba9fab0408f9513530463
Thorsten Hans <thorsten.hans@gmail.com>
2022-12-20 09:02:06 +0100

    chore(orders): Allow Disable Console Log
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/OrdersService/Configuration/OrdersServiceConfiguration.cs
+++ src/OrdersService/Configuration/OrdersServiceConfiguration.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging.Console;
+
 namespace OrdersService.Configuration;
 
 public class OrdersServiceConfiguration
@@ -10,6 +12,8 @@ public class OrdersServiceConfiguration
     public const string SectionName = "OrdersService";
     public string CreateOrderPubSubName { get; set; } = "orders";
     public string CreateOrderTopicName { get; set; } = "new_orders";
+    public string ConsoleFormatterName { get; set; } = ConsoleFormatterNames.Json;
+    public bool DisableConsoleLog { get; set; }
     public string ZipkinEndpoint { get; set; }
     public bool ExposePrometheusMetrics {get;set;}
     public string ApplicationInsightsConnectionString {get;set;}
