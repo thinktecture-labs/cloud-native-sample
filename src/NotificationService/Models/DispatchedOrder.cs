b42eae1d10e7d5013560e5646fc8a2b899d5bff1
Thorsten Hans <thorsten.hans@gmail.com>
2022-12-20 10:51:44 +0100

    chore(notification): Explicitly provide JsonPropertyName for Dapr pub/sub payload
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/NotificationService/Models/DispatchedOrder.cs
+++ src/NotificationService/Models/DispatchedOrder.cs
@@ -1,8 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace NotificationService.Models;
 
 public class DispatchedOrder
 {
+    [JsonPropertyName("orderId")]
     public string OrderId { get; set; }
+
+    [JsonPropertyName("userId")]
     public string UserId { get; set; }
+    [JsonPropertyName("userName")]
+    public string UserName { get; set; }
 }
 
