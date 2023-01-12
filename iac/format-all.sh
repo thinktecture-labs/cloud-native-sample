3c464eb0a549502e0e2cec5eb9ac566ec21a72d7
Thorsten Hans <thorsten.hans@gmail.com>
2023-01-12 09:34:46 +0100

    chore(iac): fmt recursive for Terraform
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- iac/format-all.sh
+++ iac/format-all.sh
@@ -3,20 +3,7 @@
 # simple script to re-format all terraform sources
 echo "Work, Work! 🚧"
 
-echo " - 💄 Formatting top level module"
-cd azure
-terraform fmt
-cd ..
-
-cd state
-echo " - 💄 Formatting state module"
-terraform fmt
-cd ..
-
-cd kubernetes
-echo " - 💄 Formatting Kubernetes module"
-terraform fmt
-cd ..
+terraform fmt -recursive
 
 echo "All done! ✅"
 
