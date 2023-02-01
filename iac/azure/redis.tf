e0e7eb4951ef165f69659eb546744277ea61d88f
Thorsten Hans <thorsten.hans@gmail.com>
2023-02-01 11:22:19 +0100

    chore(iac): Add Redis Cache
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

---
+++ iac/azure/redis.tf
@@ -0,0 +1,8 @@
+resource "azurerm_redis_cache" "redis" {
+  name                = "redis-cloud-native-sample-${terraform.workspace}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  capacity            = 1
+  family              = "C"
+  sku_name            = "Basic"
+}
