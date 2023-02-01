43a905466fccb2a68e9d19fbe5d0bebb36e9dc23
Thorsten Hans <thorsten.hans@gmail.com>
2023-02-01 09:28:44 +0100

    chore(*): Add developer tooling CLI (cn-sample)
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

---
+++ src/cn-sample/go.mod
@@ -0,0 +1,5 @@
+module github.com/thinktecture-labs/cloud-native-sample/cn-sample
+
+go 1.19
+
+require github.com/magefile/mage v1.14.0 // indirect
---
+++ src/cn-sample/main.go
@@ -0,0 +1,40 @@
+//go:build mage
+
+// Cloud-Native Sample Developer CLI
+package main
+
+import (
+	"os"
+
+	"github.com/magefile/mage/sh"
+)
+
+// 📦 Initialize your local machine to run the cloud-native sample
+func Init() {
+	sh.Run("docker", "plugin", "install", "grafana/loki-docker-driver:latest --alias loki --grant-all-permissions")
+}
+
+// 🚀 Start the cloud-native sample
+func Start() {
+	sh.Run("docker-compose", "up --build -d")
+}
+
+// ⚡️ Quickstart the cloud-native sample
+func Quickstart() {
+	sh.Run("docker-compose", "up -d")
+}
+
+// 🛑 Stop the cloud-native sample
+func Stop() {
+	sh.Run("docker-compose", "down")
+}
+
+// 📝 View the logs of the cloud-native sample
+func Logs() {
+	sh.Exec(make(map[string]string), os.Stdout, os.Stderr, "docker-compose", "logs", "-f")
+}
+
+// 🧹 Clean-up your loacl machine
+func CleanUp() {
+	sh.Run("docker-compose", "down --rmi all --volumes --remove-orphans")
+}
