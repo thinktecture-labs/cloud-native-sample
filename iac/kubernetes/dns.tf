74fb0928b6c53a0e1a3b2211c498cba3a8d740b4
Thorsten Hans <thorsten.hans@gmail.com>
2023-02-08 11:26:55 +0100

    chore(iac): Add DNS record for FakeSMTP
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- iac/kubernetes/dns.tf
+++ iac/kubernetes/dns.tf
@@ -33,3 +33,12 @@ resource "azurerm_dns_a_record" "dapr_dashboard" {
   ttl                 = 300
   records             = [data.kubernetes_service.ing.status.0.load_balancer.0.ingress.0.ip]
 }
+
+resource "azurerm_dns_a_record" "fakesmtp" {
+  count               = var.dns_zone.deploy_dns ? 1 : 0
+  name                = "cn-fakesmtp"
+  zone_name           = var.dns_zone.name
+  resource_group_name = var.dns_zone.resource_group_name
+  ttl                 = 300
+  records             = [data.kubernetes_service.ing.status.0.load_balancer.0.ingress.0.ip]
+}
