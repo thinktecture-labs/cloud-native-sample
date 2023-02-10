44e9dccac383813da8652025e0e28ed7094b28a7
Thorsten Hans <thorsten.hans@gmail.com>
2023-02-10 09:11:19 +0100

    chore(iac): Fix name in key (blame vscode auto-complete in TF)
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- iac/azure-secrets/servicebus.tf
+++ iac/azure-secrets/servicebus.tf
@@ -4,6 +4,6 @@ resource "kubernetes_secret" "servicebus" {
     namespace = "cloud-native-sample"
   }
   data = {
-    "coconnectionString" = var.azure_servicebus_connection_string
+    "connectionString" = var.azure_servicebus_connection_string
   }
 }
