34404903837addc741fc222f7d2fad2514ee8cae
Thorsten Hans <thorsten.hans@gmail.com>
2023-02-19 19:25:47 +0100

    chore(demo): perpare block 3 demo
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/PriceDropNotifierService/Controllers/PriceDropNotifierController.cs
+++ src/PriceDropNotifierService/Controllers/PriceDropNotifierController.cs
@@ -26,29 +26,9 @@ public class PriceDropNotifierController : ControllerBase
     [AllowAnonymous]
     public async Task<IActionResult> Notify([FromBody] CloudEvent<NotificationRequest> model)
     {
-        try
-        {
-            if (!HttpContext.Request.HasValidDaprApiToken())
-            {
-                _logger.LogWarning("OnOrderProcessed: Received invalid Dapr API token.");
-                return Unauthorized();
-            }
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error while validating Dapr API token");
-            return StatusCode((int)HttpStatusCode.InternalServerError);
-        }
-        
-        _logger.LogInformation("Received notification request for {productName} with price {price}", model.Data.ProductName, model.Data.Price);
-        var mailBody = @$"<h1>PriceDrop for {model.Data.ProductName}</h1>
-            <p>Price dropped to {model.Data.Price:F2}</p>";
-
-        await _client.InvokeBindingAsync("email", "create", mailBody, new Dictionary<string, string> {
-            {"emailTo", model.Data.Recipient},
-            {"subject", $"📉 Price drop for {model.Data.ProductName}"}
-        });
-
+        // Validate Dapr API token
+        // Send Notification
+       
         return Ok();
     }
 }
