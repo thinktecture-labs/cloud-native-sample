70b9ff0f1dbf8213d6ec497b1ac739aacb26b88e
Kenny Pflug <kenny.pflug@thinktecture.com>
2023-06-05 10:27:10 +0200

    remove logging, metrics, and tracing from PriceWatcher service
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- src/PriceWatcherService/Configuration/PriceWatcherServiceConfiguration.cs
+++ src/PriceWatcherService/Configuration/PriceWatcherServiceConfiguration.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Logging.Console;
-
 namespace PriceWatcher.Configuration;
 
 public class PriceWatcherServiceConfiguration
@@ -12,11 +10,6 @@ public class PriceWatcherServiceConfiguration
     public const string SectionName = "PriceWatcherService";
     public string PriceDropsPubSubName { get; set; } = "pricedrops";
     public string PriceDropsTopicName { get; set; } = "notifications";
-    public string ConsoleFormatterName { get; set; } = ConsoleFormatterNames.Json;
-    public bool DisableConsoleLog { get; set; }
-    public string ZipkinEndpoint { get; set; }
-    public bool ExposePrometheusMetrics {get;set;}
-    public string ApplicationInsightsConnectionString {get;set;}
-    public IdentityServerConfiguration IdentityServer { get;set; }
+    public IdentityServerConfiguration IdentityServer { get; set; }
     public Authorization Authorization { get; set; }
 }
