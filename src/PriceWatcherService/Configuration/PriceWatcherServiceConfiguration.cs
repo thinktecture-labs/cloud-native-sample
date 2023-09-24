3fa746460f03f0dc6befc5590b335ece3f8a610e
Kenny Pflug <kenny.pflug@live.de>
2023-09-24 10:41:19 +0200

    finish config and observability section
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- src/PriceWatcherService/Configuration/PriceWatcherServiceConfiguration.cs
+++ src/PriceWatcherService/Configuration/PriceWatcherServiceConfiguration.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging.Console;
+
 namespace PriceWatcher.Configuration;
 
 public class PriceWatcherServiceConfiguration
@@ -11,6 +13,11 @@ public class PriceWatcherServiceConfiguration
     public const string SectionName = "PriceWatcherService";
     public string PriceDropsPubSubName { get; set; } = "pricedrops";
     public string PriceDropsTopicName { get; set; } = "notifications";
+    public string ConsoleFormatterName { get; set; } = ConsoleFormatterNames.Json;
+    public bool DisableConsoleLog { get; set; }
+    public string ZipkinEndpoint { get; set; } = string.Empty;
+    public bool ExposePrometheusMetrics { get; set; }
+    public string ApplicationInsightsConnectionString { get; set; } = string.Empty;
     public IdentityServerConfiguration IdentityServer { get; set; }
     public Authorization Authorization { get; set; }
 }
--- src/PriceWatcherService/Extensions/WebApplicationBuilderExtensions.cs
+++ src/PriceWatcherService/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,10 +1,120 @@
+using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.IdentityModel.Tokens;
 using PriceWatcher.Configuration;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using PriceWatcher;
 
 namespace Microsoft.AspNetCore.Builder;
 
 public static class WebApplicationBuilderExtensions
 {
+    private const string ServiceName = "PriceWatcherService";
+    private static readonly string AppVersion =
+        typeof(WebApplicationBuilderExtensions)
+           .Assembly
+           .GetName()
+           .Version!.ToString();
+    
+    private static readonly Action<ResourceBuilder> ConfigureOpenTelemetryResource =
+        builder => builder.AddService(
+            ServiceName,
+            serviceVersion: AppVersion,
+            serviceInstanceId: Environment.MachineName
+        );
+    
+    public static WebApplicationBuilder ConfigureLogging(this WebApplicationBuilder builder, PriceWatcherServiceConfiguration cfg)
+    {
+        builder.Logging.ClearProviders();
+        if (System.Diagnostics.Debugger.IsAttached)
+        {
+            builder.Logging.AddDebug();
+        }
+        
+        if (!cfg.DisableConsoleLog)
+        {
+            builder.Logging.AddConsole(options =>
+            {
+                options.FormatterName = cfg.ConsoleFormatterName;
+            });
+        }
+        
+        builder.Logging.AddOpenTelemetry(options =>
+        {
+            var b = ResourceBuilder.CreateDefault();
+            ConfigureOpenTelemetryResource(b);
+            options.SetResourceBuilder(b);
+            if (!string.IsNullOrWhiteSpace(cfg.ApplicationInsightsConnectionString))
+            {
+                Console.WriteLine("Logs: Sending to Azure Monitor");
+                options.AddAzureMonitorLogExporter(o => o.ConnectionString = cfg.ApplicationInsightsConnectionString);
+            }
+        });
+        
+        return builder;
+    }
+    
+    public static WebApplicationBuilder ConfigureTracing(this WebApplicationBuilder builder, PriceWatcherServiceConfiguration cfg)
+    {
+        builder
+           .Services
+           .AddOpenTelemetry()
+           .ConfigureResource(ConfigureOpenTelemetryResource)
+           .WithTracing(options =>
+            {
+                options.AddProcessor<CustomProcessor>();
+                options
+                   .AddHttpClientInstrumentation()
+                   .AddAspNetCoreInstrumentation();
+                if (!string.IsNullOrWhiteSpace(cfg.ApplicationInsightsConnectionString))
+                {
+                    Console.WriteLine("Tracing: Adding Azure Monitor Exporter");
+                    options.AddAzureMonitorTraceExporter(
+                        o => o.ConnectionString = cfg.ApplicationInsightsConnectionString);
+                }
+
+                if (!string.IsNullOrWhiteSpace(cfg.ZipkinEndpoint))
+                {
+                    Console.WriteLine("Tracing: Adding Zipkin Exporter (" + cfg.ZipkinEndpoint + ")");
+                    options.AddZipkinExporter(o => { o.Endpoint = new (cfg.ZipkinEndpoint); });
+                }
+            });
+
+        return builder;
+    }
+    
+    public static WebApplicationBuilder ConfigureMetrics(this WebApplicationBuilder builder, PriceWatcherServiceConfiguration cfg)
+    {
+        builder
+           .Services
+           .AddOpenTelemetry()
+           .ConfigureResource(ConfigureOpenTelemetryResource)
+           .WithMetrics(options =>
+            {
+                options
+                   .AddRuntimeInstrumentation()
+                   .AddHttpClientInstrumentation()
+                   .AddAspNetCoreInstrumentation()
+                   .AddMeter(CustomMetrics.Default.Name);
+
+                if (!string.IsNullOrWhiteSpace(cfg.ApplicationInsightsConnectionString))
+                {
+                    Console.WriteLine("Metrics: Adding Azure Monitor Exporter");
+                    options.AddAzureMonitorMetricExporter(
+                        o => o.ConnectionString = cfg.ApplicationInsightsConnectionString);
+                }
+
+                if (cfg.ExposePrometheusMetrics)
+                {
+                    Console.WriteLine("Exposing Prometheus Metrics");
+                    options.AddPrometheusExporter();
+                }
+            });
+
+        return builder;
+    }
+    
     public static WebApplicationBuilder ConfigureAuthN(this WebApplicationBuilder builder, PriceWatcherServiceConfiguration cfg)
     {
         builder
